8dd3595b3e638e5c1715d0711595df6a2b63c9f5
tehZevo <tehzevo@users.noreply.github.com>
2021-12-17 00:13:00 -0500

    Add safety REQUIRE_MENTION

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const {Client, Intents} = require("discord.js");
 var TOKEN = process.env["TOKEN"]
 var RECEIVER = process.env["RECEIVER"];
 var IGNORE_SELF = process.env["IGNORE_SELF"] || true;
+var REQUIRE_MENTION = process.env["REQUIRE_MENTION"] || true;
 
 const client = new Client({intents:[Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
 
@@ -11,11 +12,34 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 })
 
+function startsWithMention(text)
+{
+  text = text.trim();
+  return text.startsWith(`<@${client.user.id}>`) || text.startsWith(`<@!${client.user.id}>`);
+}
+
+function trimMention(text)
+{
+
+}
+
 // Register an event to handle incoming messages
 client.on('messageCreate', async (msg) => {
+  var content = msg.content;
   if(msg.author == client.user && IGNORE_SELF)
   {
     return;
+  }
+  if(REQUIRE_MENTION)
+  {
+    if(startsWithMention(content))
+    {
+      content = trimMention(content)
+    }
+    else
+    {
+      return;
+    }
   }
   //send message to receiver
   var response = await protopost(RECEIVER, msg.content);
