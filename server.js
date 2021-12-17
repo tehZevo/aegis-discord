e0f003b6c7f86d089a8297df04fcb0511a723b52
tehZevo <tehzevo@users.noreply.github.com>
2021-12-16 23:55:56 -0500

    Add safety IGNORE_SELF

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var protopost = require("protopost").client;
 const {Client, Intents} = require("discord.js");
 
 var TOKEN = process.env["TOKEN"]
-
 var RECEIVER = process.env["RECEIVER"];
+var IGNORE_SELF = process.env["IGNORE_SELF"] || true;
 
 const client = new Client({intents:[Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
 
@@ -13,7 +13,10 @@ client.on('ready', () => {
 
 // Register an event to handle incoming messages
 client.on('messageCreate', async (msg) => {
-  console.log(msg)
+  if(msg.author == client.user && IGNORE_SELF)
+  {
+    return;
+  }
   //send message to receiver
   var response = await protopost(RECEIVER, msg.content);
   //if response is non-null, and non-empty, send it as a response
